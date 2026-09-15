6416ab533f5e631d381f624e4e94812e83c66481
AR123456 <coloradoz99@gmail.com>
2026-09-15 08:32:19 -0400

    update index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,6 +6,10 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require("../../assets/images/icon.png")}
+        style={{ width: 200, height: 200, borderRadius: 20 }}
+      />
     </View>
   );
 }
