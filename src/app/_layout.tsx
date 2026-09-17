4e893aa9653aaff5cb503367342b497f090afcb3
AR123456 <coloradoz99@gmail.com>
2026-09-17 12:49:16 -0400

    clerkprovider in return

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,5 +7,9 @@ if (!publishableKey) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
+  );
 }
