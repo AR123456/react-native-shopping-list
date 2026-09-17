955662b8c99e197d5003370cff3ba01711a857f9
AR123456 <coloradoz99@gmail.com>
2026-09-17 12:48:10 -0400

    more needed imports from clerk

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack, Slot } from "expo-router";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 import "../../global.css";
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 export default function RootLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
