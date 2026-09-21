4d1f25a07942bccc4de64e846aac4144371765c3
AR123456 <coloradoz99@gmail.com>
2026-09-21 09:33:54 -0400

    more corrections under index tsx

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -1,33 +1,31 @@
 // TBD content
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import {Link} from "expo-router";
-import { Show,useClerk,useUser } from '@clerk/expo';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Show, useClerk, useUser } from "@clerk/expo";
 
 export default function Page() {
-    const (user)=useUser();
-    const (signOut)=useClerk();
+  const { user } = useUser();
+  const { signOut } = useClerk();
 
   return (
     <View>
       <Text>Welcome</Text>
       <Show when="signed-out">
         <Link href="/(auth)/sign-in">
-        <Text>Sign in</Text>
+          <Text>Sign in</Text>
         </Link>
         <Link href="/(auth)/sign-up">
-        <Text>Sign up</Text>
+          <Text>Sign up</Text>
         </Link>
       </Show>
-      <Show>
-        <Text>Hello user{user?emailAddress[0].emailAddress} </Text>
-        <Pressable onPress={(()=>signOut()}>
-            <Text>Sign Out</Text>
+      <Show when="signed-in">
+        <Text>Hello user{user?.emailAddresses[0].emailAddress} </Text>
+        <Pressable onPress={() => signOut()}>
+          <Text>Sign Out</Text>
         </Pressable>
       </Show>
     </View>
-  )
+  );
 }
 
- 
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
