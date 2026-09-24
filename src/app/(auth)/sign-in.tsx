2c60eaf6c8be0ab8912e249885d108ca9ed9d507
AR123456 <coloradoz99@gmail.com>
2026-09-24 09:00:28 -0400

    adding some place holder text

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,1 +1,19 @@
 // for sign in
+// for sign in
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <Text>Sign in (placeholder)</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+});
