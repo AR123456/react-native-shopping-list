f5025629a0899a58757a06b0ed0db3704a4c6da9
AR123456 <coloradoz99@gmail.com>
2026-09-25 04:31:47 -0400

    fixing headers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,7 @@ if (!publishableKey) {
 export default function RootLayout() {
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
     </ClerkProvider>
   );
 }
